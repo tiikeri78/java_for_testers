e7c5748dd39b9fe0fa03caa542fc714ed68a4690
tiikeri78 <tmazko@yandex.ru>
2019-04-12 11:53:19 +0300

    Refactoring of MyFirstProgram.
    Function "distance" was moved from MyFirstProgram to Point.

--- sandbox/src/main/java/my/test/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/my/test/sandbox/MyFirstProgram.java
@@ -1,20 +1,11 @@
 package my.test.sandbox;
 
-import static java.lang.Math.pow;
-import static java.lang.Math.sqrt;
-
 public class MyFirstProgram {
 
 public static void main(String[] args){
 
-    Point p1 = new Point(2, 3);
-    Point p2 = new Point(4, 6);
+    Point p = new Point(2, 3, 4, 6);
 
-    System.out.println("Расстояние между точкамми с координатами " + p1.x + ", " + p1.y + " и " + p2.x + ", " + p2.y + " равно " + distance(p1, p2) + ".");
+    System.out.println("Расстояние между точкамми с координатами " + "(" + p.x1 + ", " + p.y1+ ")" + " и " + "(" + p.x2 + ", " + p.y2 + ")" + " равно " + p.distance() + ".");
  }
-
-    public static double distance(Point p1, Point p2){
-
-        return sqrt(pow((p2.x-p1.x), 2)+pow((p2.y-p1.y), 2));
-    }
 }
--- sandbox/src/main/java/my/test/sandbox/Point.java
+++ sandbox/src/main/java/my/test/sandbox/Point.java
@@ -1,13 +1,27 @@
 package my.test.sandbox;
 
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
 public class Point {
 
-    public double x;
-    public double y;
+    public double x1;
+    public double y1;
+    public double x2;
+    public double y2;
 
-    public Point(double x, double y){
 
-        this.x = x;
-        this.y = y;
+    public Point(double x1, double y1, double x2, double y2){
+
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+
+    public double distance(){
+
+        return sqrt(pow((this.x2-this.x1), 2)+pow((this.y2-this.y1), 2));
     }
 }
