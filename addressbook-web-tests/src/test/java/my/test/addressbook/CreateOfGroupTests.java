1a016cec9193944405c582f7ead8a6cc48ebab7c
tiikeri78 <tmazko@yandex.ru>
2019-04-15 10:12:13 +0300

    The "Login" was moved from @Test to @BeforeMethod

--- addressbook-web-tests/src/test/java/my/test/addressbook/CreateOfGroupTests.java
+++ addressbook-web-tests/src/test/java/my/test/addressbook/CreateOfGroupTests.java
@@ -18,10 +18,6 @@ public class CreateOfGroupTests {
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testCreateOfGroup() throws Exception {
     wd.get("http://localhost/addressbook/");
     wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys("admin");
@@ -30,6 +26,10 @@ public class CreateOfGroupTests {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testCreateOfGroup() throws Exception {
     wd.findElement(By.name("searchform")).click();
     wd.findElement(By.linkText("groups")).click();
     wd.findElement(By.name("new")).click();
