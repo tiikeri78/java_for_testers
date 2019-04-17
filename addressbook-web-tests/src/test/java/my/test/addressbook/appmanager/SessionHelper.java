e7f68611fde16efdabc7c789210537ebaeb74a1b
tiikeri78 <tmazko@yandex.ru>
2019-04-17 17:11:51 +0300

    Refactoring of class SessionHelper

--- addressbook-web-tests/src/test/java/my/test/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/my/test/addressbook/appmanager/SessionHelper.java
@@ -3,24 +3,19 @@ package my.test.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class SessionHelper {
-    private WebDriver wd;
+public class SessionHelper extends BaseHelper {
 
     public SessionHelper(WebDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     protected void login(String username, String password) {
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(username);
-        wd.findElement(By.id("LoginForm")).click();
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(password);
-        wd.findElement(By.xpath("//input[@value='Login']")).click();
+        type(By.name("user"), username);
+        type(By.name("pass"), password);
+        click(By.xpath("//input[@value='Login']"));
     }
 
     public void logout() {
-        wd.findElement(By.linkText("Logout")).click();
+        click(By.linkText("Logout"));
     }
 }
