a92b5a5e84f029168b32ffdaf74d1eea1b202eee
tiikeri78 <tmazko@yandex.ru>
2019-04-20 17:21:49 +0300

    Added the ability to run tests in different browsers.

--- addressbook-web-tests/src/test/java/my/test/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/my/test/addressbook/appmanager/ApplicationManager.java
@@ -1,6 +1,10 @@
 package my.test.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -10,8 +14,20 @@ public class ApplicationManager {
     private ContactHelper contactHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
+        if (browser.equals(BrowserType.FIREFOX)){
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)){
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.IE)){
+            wd = new InternetExplorerDriver();
+        }
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         groupHelper = new GroupHelper(wd);
--- addressbook-web-tests/src/test/java/my/test/addressbook/test/TestBase.java
+++ addressbook-web-tests/src/test/java/my/test/addressbook/test/TestBase.java
@@ -1,17 +1,16 @@
 package my.test.addressbook.test;
 
 import my.test.addressbook.appmanager.ApplicationManager;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
-        app.wd = new FirefoxDriver();
         app.init();
     }
 
