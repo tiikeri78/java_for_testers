a0c0794ea5b803d2b88c7e3cab7c4c18be572efe
tiikeri78 <tmazko@yandex.ru>
2019-04-28 20:18:29 +0300

    Refactoring of EditGroupTests

--- addressbook-web-tests/src/test/java/my/test/addressbook/test/EditGroupTests.java
+++ addressbook-web-tests/src/test/java/my/test/addressbook/test/EditGroupTests.java
@@ -17,10 +17,15 @@ public class EditGroupTests extends TestBase {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().editGroup();
-        app.getGroupHelper().fillGroupForm(new GroupData("Workers", "work", "Testers1"));
+        GroupData group = new GroupData(before.get(before.size()-1).getId(),"Workers", "work", "Testers1");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().updateGroup();
         app.getNavigationHelper().gotoGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), after.size());
+
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals(before, after);
     }
 }
