543ce124684a9eddfa6ee214083e91c00e7a5231
tiikeri78 <tmazko@yandex.ru>
2019-05-20 09:46:55 +0300

    Задание 15

--- addressbook-web-tests/src/test/java/my/test/addressbook/test/DeleteContactTest.java
+++ addressbook-web-tests/src/test/java/my/test/addressbook/test/DeleteContactTest.java
@@ -6,6 +6,8 @@ import my.test.addressbook.model.GroupData;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.File;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.testng.Assert.assertEquals;
@@ -20,8 +22,9 @@ public class DeleteContactTest extends TestBase{
     }
     if (app.db().contacts().size() == 0) {
       app.goTo().contactPage();
+      File photo = new File("src/test/resources/frog.jpg");
       app.contact().create(new ContactData().withFirstname("Zelda").withLastname("Smith").withAddress("Nevada").withMobileNumber("+195432567")
-              .withEmail("krasotka@mail.ry").withGroup("Test1"), true);
+              .withEmail("krasotka@mail.ry").withGroup("Test1").withPhoto(photo), true);
     }
   }
 
