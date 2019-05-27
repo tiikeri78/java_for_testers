5ce8a7e6657f60221227df31b4f202ea35e56527
tiikeri78 <tmazko@yandex.ru>
2019-05-27 11:29:16 +0300

    В тесты Mantis добавлен BaseHelper

---
+++ mantis-tests/src/test/java/my/test/mantis/appmanager/BaseHelper.java
@@ -0,0 +1,57 @@
+package my.test.mantis.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+
+public class BaseHelper {
+    protected ApplicationManager app;
+    protected WebDriver wd;
+
+    public BaseHelper(ApplicationManager app) {
+        this.app = app;
+        this.wd = wd;
+    }
+
+    protected void click(By locator) {
+        wd.findElement(locator).click();
+    }
+
+    protected void type(By locator, String text) {
+        click(locator);
+        if (text != null) {
+            String existingText = wd.findElement(locator).getAttribute("value");
+            if (!text.equals(existingText)) {
+                wd.findElement(locator).clear();
+                wd.findElement(locator).sendKeys(text);
+            }
+        }
+    }
+
+    protected void attachFile(By locator, File file) {
+        if (file != null) {
+            wd.findElement(locator).sendKeys(file.getAbsolutePath());
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    protected boolean isElementPresent(By locator) {
+        try {
+            wd.findElement(locator);
+            return true;
+        } catch (NoSuchElementException ex) {
+            return false;
+        }
+    }
+}
--- mantis-tests/src/test/java/my/test/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/my/test/mantis/appmanager/RegistrationHelper.java
@@ -1,17 +1,18 @@
 package my.test.mantis.appmanager;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
 
-public class RegistrationHelper {
-    private final ApplicationManager app;
-    private WebDriver wd;
+public class RegistrationHelper extends BaseHelper{
 
     public RegistrationHelper(ApplicationManager app) {
-        this.app = app;
-        wd = app.getDriver();
+        super(app);
     }
 
     public void start(String username, String email) {
+
         wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
+        type(By.name("username"), username);
+        type(By.name("email"), email);
+        click(By.cssSelector("input[value='Sighup']"));
     }
 }
