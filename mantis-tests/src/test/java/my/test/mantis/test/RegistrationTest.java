8e432673610f9bf2ead14540948bbf6a88dddd62
tiikeri78 <tmazko@yandex.ru>
2019-05-27 17:49:06 +0300

    Добавлен MailHelper

--- mantis-tests/src/test/java/my/test/mantis/test/RegistrationTest.java
+++ mantis-tests/src/test/java/my/test/mantis/test/RegistrationTest.java
@@ -1,11 +1,23 @@
 package my.test.mantis.test;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class RegistrationTest extends TestBase {
 
+    @BeforeMethod
+    public void startMailServer() {
+        app.mail().start();
+    }
+
     @Test
     public void testRegistration(){
         app.registration().start("user", "441@mail.ru");
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void stopMailServer() {
+        app.mail().stop();
     }
 }
