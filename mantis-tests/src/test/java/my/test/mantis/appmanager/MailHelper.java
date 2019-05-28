3ec37a0453391710d77f2b290c2b411e9d1ab54d
mazko <tmazko@yandex.ru>
2019-05-28 17:30:23 +0300

    Проблема с MailHelper

--- mantis-tests/src/test/java/my/test/mantis/appmanager/MailHelper.java
+++ mantis-tests/src/test/java/my/test/mantis/appmanager/MailHelper.java
@@ -2,12 +2,11 @@ package my.test.mantis.appmanager;
 
 import org.subethamail.wiser.Wiser;
 import org.subethamail.wiser.WiserMessage;
-
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.io.IOException;
-import java.util.List;
 import java.util.stream.Collectors;
+
 
 public class MailHelper {
     private ApplicationManager app;
@@ -15,21 +14,23 @@ public class MailHelper {
 
     public MailHelper(ApplicationManager app) {
         this.app = app;
-        wiser = new Wiser();
+        wiser = new Wiser;
     }
 
     public List<MailMessage> waitForMail(int count, long timeout) throws MessagingException, IOException {
         long start = System.currentTimeMillis();
         while (System.currentTimeMillis() < start + timeout) {
-            if (wiser.getMessages().stream().map((m) -> toModelMail(m)).collect(Collectors.toList())) ;
+            if (wiser.getMessages().size() >= count) {
+                return wiser.getMessages().stream().map((m) -> toModelMail(m)).collect(Collectors.toList());
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            throw new Error("No mail :(");
         }
-        throw new Error("No mail :(");
-    }
 
 
     public static MailMessage toModelMail(WiserMessage m) {
