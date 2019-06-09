3ddceca3d2d49bca381b5013d660c0b6f1182e18
tiikeri78 <tmazko@yandex.ru>
2019-06-09 09:40:22 +0300

    Добавлен запуск через Jenkins тестов Sandbox

--- sandbox/build/scripts/sandbox.bat
+++ sandbox/build/scripts/sandbox.bat
@@ -79,7 +79,7 @@ set CMD_LINE_ARGS=%*
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\lib\sandbox.jar
+set CLASSPATH=%APP_HOME%\lib\sandbox.jar;%APP_HOME%\lib\testng-6.14.3.jar;%APP_HOME%\lib\jcommander-1.72.jar;%APP_HOME%\lib\bsh-2.0b6.jar
 
 @rem Execute sandbox
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %SANDBOX_OPTS%  -classpath "%CLASSPATH%" MyFirstProgram %CMD_LINE_ARGS%
