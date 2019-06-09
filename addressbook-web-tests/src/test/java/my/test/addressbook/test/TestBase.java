249174683accf0a950ce3c21bb873b9acd6a19f2
tiikeri78 <tmazko@yandex.ru>
2019-06-09 13:42:42 +0300

    Исправлен протокол из-за бага в Gradle

--- addressbook-web-tests/src/test/java/my/test/addressbook/test/TestBase.java
+++ addressbook-web-tests/src/test/java/my/test/addressbook/test/TestBase.java
@@ -23,7 +23,7 @@ public class TestBase {
 
     Logger logger = LoggerFactory.getLogger(TestBase.class);
 
-    public static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+    public static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
 
     @BeforeSuite(alwaysRun = true)
     public void setUp() throws Exception {
