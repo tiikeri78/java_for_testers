dfcb953ffe37fa4bf79cb599a4f88decd3dcd1e4
tiikeri78 <tmazko@yandex.ru>
2019-06-10 22:55:38 +0300

    Задание 20

--- bugify-tests/src/test/java/my/test/bugify/test/TestBase.java
+++ bugify-tests/src/test/java/my/test/bugify/test/TestBase.java
@@ -1,18 +1,13 @@
 package my.test.bugify.test;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
-import com.google.gson.reflect.TypeToken;
-import my.test.bugify.model.Issue;
 import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
 import org.testng.SkipException;
 import org.testng.annotations.BeforeTest;
 
 import java.io.IOException;
-import java.util.Objects;
-import java.util.Set;
 
 public class TestBase {
 
@@ -22,13 +17,11 @@ public class TestBase {
 
     @BeforeTest
     public boolean isIssueOpen(int issueId) throws IOException {
-        String json = getExecutor().execute(Request.Get("http://bugify.stqa.ru/api/issues.json")).returnContent().asString();
+        String json = getExecutor().execute(Request.Get("http://bugify.stqa.ru/api/issues/" + issueId + ".json")).returnContent().asString();
         JsonElement parsed = new JsonParser().parse(json);
-        JsonElement issues = parsed.getAsJsonObject().get("issues");
-        Set<Issue> newIssues = new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
-        Issue issue = newIssues.stream().filter(data -> Objects.equals(data.getId(), issueId)).findFirst().get();
+        String issueStatus = parsed.getAsJsonObject().get("status").getAsString();
 
-        if (issue.getStatus().equals("resolved")) {
+        if (issueStatus.equals("resolved")) {
             return false;
         } else return true;
     }
