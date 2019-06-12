48b4991cde36371c56ab75b9bda8ee764f35ba2c
tiikeri78 <tmazko@yandex.ru>
2019-06-12 09:02:33 +0300

    Задание 20

--- bugify-tests/src/test/java/my/test/bugify/test/TestBase.java
+++ bugify-tests/src/test/java/my/test/bugify/test/TestBase.java
@@ -1,9 +1,7 @@
 package my.test.bugify.test;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
-import my.test.bugify.model.Issue;
 import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
 import org.testng.SkipException;
@@ -18,18 +16,14 @@ public class TestBase {
 
     public boolean isIssueOpen(int issueId) throws IOException {
 
-        String issueStatus = getIssueData(issueId).getState_name();
+        String json = getExecutor().execute(Request.Get("http://bugify.stqa.ru/api/issues/" + issueId + ".json")).returnContent().asString();
+        JsonElement parsed = new JsonParser().parse(json);
+        JsonElement issues = parsed.getAsJsonObject().get("issues");
+        String issueStatus = issues.getAsJsonObject().get("state_name").getAsString();
 
         if ((issueStatus.equals("resolved")) || (issueStatus.equals("closed")) || (issueStatus.equals("deleted"))) {
             return false;
         } else return true;
-    }
-
-    private Issue getIssueData(int issueId) throws IOException {
-        String json = getExecutor().execute(Request.Get("http://bugify.stqa.ru/api/issues/" + issueId + ".json")).returnContent().asString();
-        JsonElement parsed = new JsonParser().parse(json);
-        JsonElement issues = parsed.getAsJsonObject().get("issues");
-        return new Gson().fromJson(issues, Issue.class);
     }
 
     public void skipIfNotFixed(int issueId) throws IOException {
