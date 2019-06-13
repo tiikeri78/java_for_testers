1e3beff14fe6903149f7ec7335ac30f549c88aa4
tiikeri78 <tmazko@yandex.ru>
2019-06-13 10:18:22 +0300

    Задание 20

--- bugify-tests/src/test/java/my/test/bugify/test/TestBase.java
+++ bugify-tests/src/test/java/my/test/bugify/test/TestBase.java
@@ -1,12 +1,16 @@
 package my.test.bugify.test;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import my.test.bugify.model.Issue;
 import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
 import org.testng.SkipException;
 
 import java.io.IOException;
+import java.util.List;
 
 public class TestBase {
 
@@ -19,11 +23,12 @@ public class TestBase {
         String json = getExecutor().execute(Request.Get("http://bugify.stqa.ru/api/issues/" + issueId + ".json")).returnContent().asString();
         JsonElement parsed = new JsonParser().parse(json);
         JsonElement issues = parsed.getAsJsonObject().get("issues");
-        String issueStatus = issues.getAsJsonObject().get("state_name").getAsString();
+        List<Issue> issueData = new Gson().fromJson(issues, new TypeToken<List<Issue>>() {}.getType());
+        String issueStatus = issueData.get(0).getState_name();
 
         if ((issueStatus.equals("resolved")) || (issueStatus.equals("closed")) || (issueStatus.equals("deleted"))) {
-            return false;
-        } else return true;
+            return true;
+        } else return false;
     }
 
     public void skipIfNotFixed(int issueId) throws IOException {
